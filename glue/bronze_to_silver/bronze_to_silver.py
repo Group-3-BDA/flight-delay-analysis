c5974c65c37e43a96eab6753c2fde42e36a3e9e6
Abdul Aman <jackspear7878@gmail.com>
2026-07-23 17:04:32 +0530

    Updated bronze_to_silver.py script

--- glue/bronze_to_silver/bronze_to_silver.py
+++ glue/bronze_to_silver/bronze_to_silver.py
@@ -71,6 +71,7 @@ def read_bronze_data(spark):
         spark.read
         .format(config.READ_FORMAT)
         .options(**config.CSV_OPTIONS)
+        .option("recursiveFileLookup", "true")
         .load(config.BRONZE_PATH)
     )
 
