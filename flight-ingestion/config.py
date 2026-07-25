b4f9b2b196e1a0c37b639b62ecbd7d9c26dd0d15
virajdandekar1999 <dandekarviraj1999@gmail.com>
2026-07-25 23:53:51 +0530

    Create config.py

---
+++ flight-ingestion/config.py
@@ -0,0 +1,7 @@
+DOWNLOAD_FOLDER = "/home/ec2-user/flight_data"
+
+EXTRACT_FOLDER = "/home/ec2-user/"
+
+RAW_BUCKET = "viraj-final-project"
+
+KAGGLE_DATASET = "rachit753patel/airline-flight-data-2020-2025"
