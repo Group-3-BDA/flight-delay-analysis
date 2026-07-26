457ce2ca2a0f33324b1850c811a35b7b49d477e8
virajdandekar1999 <dandekarviraj1999@gmail.com>
2026-07-26 10:05:14 +0530

    Update config.py

--- flight-ingestion/config.py
+++ flight-ingestion/config.py
@@ -2,6 +2,8 @@ DOWNLOAD_FOLDER = "/home/ec2-user/flight_data"
 
 EXTRACT_FOLDER = "/home/ec2-user/"
 
-RAW_BUCKET = "viraj-final-project"
+PROCESSED_BUCKET = "airline-dataset-2020-2025/"
+
+S3_PREFIX = "Bronze"
 
 KAGGLE_DATASET = "rachit753patel/airline-flight-data-2020-2025"
