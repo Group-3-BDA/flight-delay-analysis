d0c4ad4738a920b6b2977419b475832dd0058eaf
Abdul Aman <jackspear7878@gmail.com>
2026-08-09 13:10:03 +0530

    Delete athena/sql/06_dim_airline_slicer.sql

--- athena/sql/06_dim_airline_slicer.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW flight_gold.dim_airline_slicer AS
-
-SELECT DISTINCT
-    AirlineCode,
-    AirlineName,
-    AirlineLabel
-FROM flight_gold.viz_reliability_analytics
-ORDER BY AirlineName;
