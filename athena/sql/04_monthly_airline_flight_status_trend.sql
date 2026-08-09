a59c51910b8a611308043b4fccc0429c08cda3a0
Abdul Aman <jackspear7878@gmail.com>
2026-08-09 13:53:39 +0530

    Update 04_monthly_airline_flight_status_trend.sql

--- athena/sql/04_monthly_airline_flight_status_trend.sql
+++ athena/sql/04_monthly_airline_flight_status_trend.sql
@@ -73,6 +73,3 @@ GROUP BY
     AirlineCode,
     AirlineName,
     AirlineLabel;
----------------------------------------------------------
-AIRPORT PERFORMANCE:
---------------------------------------------------------
